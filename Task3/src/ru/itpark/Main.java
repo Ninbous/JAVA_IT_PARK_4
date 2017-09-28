f6de5611a7e765b7c7a494b184977f10ecce09d1
student1 <lot93@mail.ru>
2017-09-28 21:03:34 +0300

    Task 3

---
+++ Task3/src/ru/itpark/Main.java
@@ -0,0 +1,29 @@
+package ru.itpark;
+import static java.lang.Math.*;
+public class Main {
+
+    public static double f(double x) {
+        return cos(x);
+    }
+
+    public static double integrate(double a, double b, int n) {
+        double result = 0;
+        // рассчитать ширину разбиения
+        double h = abs(a - b) / n;
+        for (double x = a; x <= b; x = x + h) {
+            result = result + f(x) * h;
+        }
+        return result;
+    }
+
+    public static int sum(int a, int b) {
+        int result = a + b;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        double integrateOfSin =
+                integrate(0.1, PI, 1000000);
+        System.out.println(integrateOfSin);
+    }
+}
