073871403252dc8042a1967fb5a159079e954528
Ninbous <Nibous@mail.ru>
2017-09-29 19:11:58 +0300

    Modified Task3 with sort fucntion

--- Homeworks/Task2/Storage - ДЗ/src/com/company/Main.java
+++ Homeworks/Task2/Storage - ДЗ/src/com/company/Main.java
@@ -6,19 +6,17 @@ public class Main {
 
     static Scanner scanner;
 
-    static int[] storage ;
+    static int[] storage;
     static int count;
 
-    static void showArray()
-    {
+    static void showArray() {
         System.out.println("В массиве " + count + " элемент(а/ов)");
         System.out.println("Длина массива " + storage.length);
         for (int i : storage) System.out.print(i + " ");
         System.out.println("");
     }
 
-    static void addToForward()
-    {
+    static void addToForward() {
         System.out.println("Добавить в начало - введите число:");
         int num = scanner.nextInt();
         if (num != 0) {
@@ -31,17 +29,16 @@ public class Main {
         }
     }
 
-    static void addToBack()
-    {
+    static void addToBack() {
         System.out.println("Добавить в конец - введите число:");
         int num2 = scanner.nextInt();
         if (num2 != 0) {
-            for (int i = 0;i<storage.length;i++) {
+            for (int i = 0; i < storage.length; i++) {
                 if (storage[i] == 0) {
                     storage[i] = num2;
                     count++;
                     break;
-                } else if (i==99){
+                } else if (i == 99) {
                     System.out.println("Хранилище заполнено");
                 }
             }
@@ -50,8 +47,7 @@ public class Main {
         }
     }
 
-    static void deleteOnIndex()
-    {
+    static void deleteOnIndex() {
         System.out.println("Удаление по позиции - введите индекс в массиве");
         int index = scanner.nextInt();
         for (int j = 0; j < storage.length - index - 1; j++) {
@@ -62,6 +58,23 @@ public class Main {
         if (count < 0) count = 0;
     }
 
+    static void sortArray() {
+        for(int j = 0; j<storage.length; j++) {
+            int lowestValue = Integer.MAX_VALUE;
+            int i = 0;
+            int index = 0;
+            for (; j+i < storage.length; i++) {
+                if (storage[j+i] < lowestValue) {
+                    lowestValue = storage[j+i];
+                    index = j + i;
+                }
+            }
+            int temp = storage[j];
+            storage[j] = lowestValue;
+            storage[index] = temp;
+        }
+    }
+
     public static void main(String[] args) {
 
         System.out.println("МЕНЮ:");
@@ -69,7 +82,8 @@ public class Main {
         System.out.println("2. Добавить в начало");
         System.out.println("3. Добавить в конец");
         System.out.println("4. Удалить по позиции");
-        System.out.println("5. Выход");
+        System.out.println("5. Сортировать массив");
+        System.out.println("6. Выход");
 
         scanner = new Scanner(System.in);
 
@@ -94,6 +108,9 @@ public class Main {
                     deleteOnIndex();
                     break;
                 case 5:
+                    sortArray();
+                    break;
+                case 6:
                     System.out.println("Выход");
                     System.exit(0);
                     break;
