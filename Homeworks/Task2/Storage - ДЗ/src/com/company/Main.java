794a6547ad5ecb0e31f96ab3c46f6e04fb808c2a
Ninbous <Nibous@mail.ru>
2017-09-29 18:28:37 +0300

    Modified Task2 with functions

--- Homeworks/Task2/Storage - ДЗ/src/com/company/Main.java
+++ Homeworks/Task2/Storage - ДЗ/src/com/company/Main.java
@@ -4,6 +4,64 @@ import java.util.Scanner;
 
 public class Main {
 
+    static Scanner scanner;
+
+    static int[] storage ;
+    static int count;
+
+    static void showArray()
+    {
+        System.out.println("В массиве " + count + " элемент(а/ов)");
+        System.out.println("Длина массива " + storage.length);
+        for (int i : storage) System.out.print(i + " ");
+        System.out.println("");
+    }
+
+    static void addToForward()
+    {
+        System.out.println("Добавить в начало - введите число:");
+        int num = scanner.nextInt();
+        if (num != 0) {
+            int[] temp = storage.clone();
+            storage[0] = num;
+            System.arraycopy(temp, 0, storage, 1, temp.length - 1);
+            count++;
+        } else {
+            System.out.println("Ошибка - Число не должно быть равно 0!");
+        }
+    }
+
+    static void addToBack()
+    {
+        System.out.println("Добавить в конец - введите число:");
+        int num2 = scanner.nextInt();
+        if (num2 != 0) {
+            for (int i = 0;i<storage.length;i++) {
+                if (storage[i] == 0) {
+                    storage[i] = num2;
+                    count++;
+                    break;
+                } else if (i==99){
+                    System.out.println("Хранилище заполнено");
+                }
+            }
+        } else {
+            System.out.println("Ошибка - Число не должно быть равно 0!");
+        }
+    }
+
+    static void deleteOnIndex()
+    {
+        System.out.println("Удаление по позиции - введите индекс в массиве");
+        int index = scanner.nextInt();
+        for (int j = 0; j < storage.length - index - 1; j++) {
+            storage[index + j] = storage[index + j + 1];
+        }
+        storage[99] = 0;
+        count--;
+        if (count < 0) count = 0;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("МЕНЮ:");
@@ -13,10 +71,10 @@ public class Main {
         System.out.println("4. Удалить по позиции");
         System.out.println("5. Выход");
 
-        Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
 
-        int[] storage = new int[100];
-        int count = 0;
+        storage = new int[100];
+        count = 0;
 
         while (true) {
             System.out.println("Введите команду");
@@ -24,49 +82,16 @@ public class Main {
 
             switch (command) {
                 case 1:
-                    System.out.println("В массиве " + count + " элемент(а/ов)");
-                    System.out.println("Длина массива " + storage.length);
-                    for (int i : storage) System.out.print(i + " ");
-                    System.out.println("");
+                    showArray();
                     break;
                 case 2:
-                    System.out.println("Добавить в начало - введите число:");
-                    int num = scanner.nextInt();
-                    if (num != 0) {
-                        int[] temp = storage.clone();
-                        storage[0] = num;
-                        System.arraycopy(temp, 0, storage, 1, temp.length - 1);
-                        count++;
-                    } else {
-                        System.out.println("Ошибка - Число не должно быть равно 0!");
-                    }
+                    addToForward();
                     break;
                 case 3:
-                    System.out.println("Добавить в конец - введите число:");
-                    int num2 = scanner.nextInt();
-                    if (num2 != 0) {
-                        for (int i = 0;i<storage.length;i++) {
-                            if (storage[i] == 0) {
-                                storage[i] = num2;
-                                count++;
-                                break;
-                            } else if (i==99){
-                                System.out.println("Хранилище заполнено");
-                            }
-                        }
-                    } else {
-                        System.out.println("Ошибка - Число не должно быть равно 0!");
-                    }
+                    addToBack();
                     break;
                 case 4:
-                    System.out.println("Удаление по позиции - введите индекс в массиве");
-                    int index = scanner.nextInt();
-                    for (int j = 0; j < storage.length - index - 1; j++) {
-                        storage[index + j] = storage[index + j + 1];
-                    }
-                    storage[99] = 0;
-                    count--;
-                    if (count < 0) count = 0;
+                    deleteOnIndex();
                     break;
                 case 5:
                     System.out.println("Выход");
